4a7706e5d9b2b7be990780848bf3e042be442ccc
Jonathan Iqueda <jonathaniqueda@me.com>
2018-04-07 16:09:37 -0300

    teste de email

--- app/Mail/ContactEmail.php
+++ app/Mail/ContactEmail.php
@@ -31,7 +31,8 @@ class ContactEmail extends Mailable
     public function build()
     {
         return $this->subject('[Privilège Translation] Nova mensagem de contato')
-            ->to('comercial@privilege-ts.com.br', 'Contato Privilège')
+//            ->to('comercial@privilege-ts.com.br', 'Contato Privilège')
+            ->to('japaxiqueda@gmail.com', 'Contato Privilège')
             ->from($this->data['email'], $this->data['name'])
             ->view('email.contact', ['data' => $this->data]);
     }
