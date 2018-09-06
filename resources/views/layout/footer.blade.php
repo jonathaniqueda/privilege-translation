c9823b0a0b72a2d70d9a55ef5efa702db380f911
Jonathan Iqueda <jonathaniqueda@me.com>
2018-09-05 23:13:20 -0300

    change address

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -22,10 +22,16 @@
                 </ul>
                 <p class="whitecolor top30">Copyright &copy; {{ date('Y') }} Privilege Translations.</p>
                 <p class="whitecolor top30">
+                    Avenida Ernani do Amaral Peixoto, 207 - sala 1301<br/>
+                    Niterói - Rio de Janeiro<br/>
+                    Edifício Albert Sabin<br/>
+                    Telefones: (+55 21) 2717-0801 / 9-6493-4783
+                </p>
+                <p class="whitecolor top30">
                     Rua Venceslau Brás, 16 - 13 andar - Sala 134A<br/>
                     São Paulo - SP <br/>
-                    CEP: 01016-000 <br />
-                    Tel: +(55 11) 3242-6969
+                    CEP: 01016-000 <br/>
+                    Telefones: +(55 11) 3242-6969
                 </p>
                 <p class="whitecolor top30 made-by">
                     @lang('general.made_by') <a href="https://seuguru.digital/" target="_blank">seuguru.digital</a>.
